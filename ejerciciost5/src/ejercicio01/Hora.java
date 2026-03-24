b75a3f8f2b9fa52d97daaf6066c58068f08c6a1f
alvaro.vizuete <alvaro.vizuete@iesnervion.es>
2026-03-24 13:27:41 +0100

    prueba

---
+++ ejerciciost5/src/ejercicio01/Hora.java
@@ -0,0 +1,19 @@
+package ejercicio01;
+
+public class Hora {
+    private int hora;
+    private int minuto;
+
+    public Hora(int hora, int minuto) {
+        this.hora = hora;
+        this.minuto = minuto;
+    }
+
+    static void main() {
+
+
+
+
+
+    }
+}
